b8edd40a10576268c02018269db8c92200bc27f4
mfatihrabbani <mfatihrabbani@gmail.com>
2022-08-01 21:15:55 +0700

    Make config

---
+++ src/config/databaseConfig.js
@@ -0,0 +1,16 @@
+import Sequelize from "sequelize"
+
+const sequelize = new Sequelize("toko_online", "root", "root", {
+	"host": "localhost",
+	"dialect": "mysql",
+})
+
+try{
+	sequelize.connect(() => {
+		console.log("Database connect")
+	})
+}catch(err){
+	console.log("Database failed connect")
+}
+
+export default sequelize
