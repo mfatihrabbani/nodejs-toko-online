4ade6bf1d916152c4ec7d914998e8ae972c3bf8b
mfatihrabbani <mfatihrabbani@gmail.com>
2022-08-02 20:51:36 +0700

    routing api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import express from "express"
+import bodyParser from "body-parser"
+import router from "./routes.js"
 
 const app = express()
 
-app.get("/", (req, res) => {
-	res.send("Hello")
-})
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.use(express.json());
+
+app.use("/api", router)
 
 app.use((err, req, res, next) => {
-	if(err)
+	if(err){
+		errorMessage = err.message || "Something error"
+		errorStatus = err.status || 500
+		res.status(errorStatus).json({status: "Failed", code: errorStatus, message: errorMessage})
+	}else{
+		next()
+	}
 })
 
 app.listen(3000, (err) => {
