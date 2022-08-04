ecd57c029dd0639c8cdd78f97ebe9a7eb2c7cd7c
mfatihrabbani <mfatihrabbani@gmail.com>
2022-08-04 20:35:33 +0700

    add new router

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,12 @@
 import express from "express"
 import users from "./users/usersRoute.js"
+import products from "./products/productsRoute.js"
+import categorys from "./categorys/categorysRoute.js"
+
 const router = express.Router()
 
 router.use("/users", users)
+router.use("/products", products)
+router.use("/categorys", categorys)
 
 export default router
