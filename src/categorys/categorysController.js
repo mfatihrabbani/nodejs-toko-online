36a680593b528a195297b2e8508f094ce9028664
mfatihrabbani <mfatihrabbani@gmail.com>
2022-08-09 21:00:31 +0700

    fix error hancle

--- src/categorys/categorysController.js
+++ src/categorys/categorysController.js
@@ -4,6 +4,7 @@ export const postCategory = async (req, res, next) => {
 	const {categoryName} = req.body
 	try{
 		const result = await createCategory(categoryName)
+		if (result.status != undefined) return next(result) 
 		return res.status(201).json({status: "Success", code: 201, data: result})
 	}catch(error){
 		return next(error)
