cbe06696b73cdf2b74135945dec9a2fe3a8a3c71
mfatihrabbani <mfatihrabbani@gmail.com>
2022-08-09 21:01:07 +0700

    adding carts api routes

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import express from "express"
 import users from "./users/usersRoute.js"
 import products from "./products/productsRoute.js"
 import categorys from "./categorys/categorysRoute.js"
+import carts from "./carts/cartsRoute.js"
 
 const router = express.Router()
 
 router.use("/users", users)
 router.use("/products", products)
 router.use("/categorys", categorys)
+router.use("/carts", carts)
 
 export default router
