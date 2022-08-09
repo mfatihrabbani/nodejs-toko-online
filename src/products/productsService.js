26c750a01282b3955f7991cbc01fdfb0aeac2040
mfatihrabbani <mfatihrabbani@gmail.com>
2022-08-09 21:00:02 +0700

    error handle

--- src/products/productsService.js
+++ src/products/productsService.js
@@ -6,7 +6,7 @@ import uniqid from "uniqid"
 
 export const createProduct = async (products) => {
 	const {productName, productDesc, productPrice, productStock, productCategory} = products
-	if(!productName, !productPrice, !productCategory, !productStock) return createError("Please check again your field", 401)
+	if(!productName || !productPrice || !productCategory || !productStock) return createError("Please check again your field", 401)
 	if(productPrice < 0) return createError("Price can't under 0", 401)
 	try{
 		const checkCategory = await Categorys.findOne({where: {category_name: productCategory}})
