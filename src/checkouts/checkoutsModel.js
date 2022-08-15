c3546af62286d8b4b277267d0bd436b75ef3c8df
mfatihrabbani <mfatihrabbani@gmail.com>
2022-08-15 19:55:05 +0700

    checkouts model

---
+++ src/checkouts/checkoutsModel.js
@@ -0,0 +1,27 @@
+import sequelize from "../config/databaseConfig.js"
+import Orders from "../orders/ordersModel.js"
+import Users from "../users/usersModel.js"
+import {DataTypes} from "sequelize"
+
+const Checkouts = sequelize.define("checkouts_item", {
+	id_checkouts: {
+		type: DataTypes.STRING,
+		allowNull: false
+	},
+	id_orders: {
+		type: DataTypes.STRING,
+		allowNull: false
+	},
+	id_users: {
+		type: DataTypes.STRING,
+		allowNull: false
+	}
+})
+
+Checkouts.belongsTo(Orders, {foreignKey: "id_orders"})
+Checkouts.belongsTo(Users, {foreignKey: "id_users"})
+
+
+Checkouts.sync({alter: true})
+
+export default Checkouts
