a6b048927e74adf307745d47ba132fbc10272061
mfatihrabbani <mfatihrabbani@gmail.com>
2022-08-18 21:36:41 +0700

    bash order api

--- testBash/orderBash.sh
+++ testBash/orderBash.sh
@@ -2,4 +2,4 @@ curl http://localhost:3000/api/orders \
 --include \
 --header "Content-Type: application/json" \
 --request "POST" \
---data '{"userId": "1kjqhp6d8l6c7mupp", "productId" : "1kjqhp2tol6hx7kgc", "quantity": 2, "payment": "OVO"}'
+--data '{"userId": "1kjqhp6d8l6c7mupp", "productId" : "1kjqhp2tol6hx7kgc", "quantity": 2, "payment": "ID_DANA"}'
