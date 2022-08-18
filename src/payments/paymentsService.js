16e737859c2c0639a8799afc504d68b5f76dddb6
mfatihrabbani <mfatihrabbani@gmail.com>
2022-08-18 21:35:20 +0700

    fix service

--- src/payments/paymentsService.js
+++ src/payments/paymentsService.js
@@ -3,8 +3,11 @@ import dotenv from "dotenv"
 import {createError} from "../utils/error.js"
 dotenv.config()
 
+const apiKey = process.env.XENDITKEY
+
+
 const x = new Xendit({
-	secretKey: process.env.XENDITKEY
+	secretKey: process.env.XENDITKEY || "xnd_development_uGTi0jcuVx1YlkDukuHOCzf6MaBYByqwfH5ViW7ZUT1jhnJDFp7tiMEmR1KG2sh6"
 });
 const { EWallet } = x;
 const ewalletSpecificOptions = {};
@@ -12,32 +15,35 @@ const ew = new EWallet(ewalletSpecificOptions);
 
 export const createChargeEwallet = async (data) => {
 	const {id_products, payment, total} = data
-	if(!orderId || !userId || !payment) return createError("Cannot make charge", 401)
+	console.log(apiKey)
+	console.log( payment)
+	if(!total || !payment) return createError("Cannot make charge", 401)
 		try{
-			switch(method){
-			case "ID_OVO":
-				const charge = await ew.createEWalletCharge({
-					referenceID: id_products,
-			  		currency: 'IDR',
-			  		amount: total,
-			  		checkoutMethod: 'ONE_TIME_PAYMENT',
-			  		channelCode: 'ID_OVO',
-			  		channelProperties: {
- 	   					mobileNumber: '+6281234567890',
-  					}
-			})
-			break
+			switch(payment){
+				case "ID_OVO":
+					var charge = await ew.createEWalletCharge({
+						referenceID: id_products,
+				  		currency: 'IDR',
+				  		amount: total,
+				  		checkoutMethod: 'ONE_TIME_PAYMENT',
+				  		channelCode: 'ID_OVO',
+				  		channelProperties: {
+	 	   					mobileNumber: '+6281234567890',
+	  					}
+				})
+				break
 			case "ID_DANA":
-				const charge = await ew.createEWalletCharge({
+				var charge = await ew.createEWalletCharge({
 					referenceID: id_products,
 			  		currency: 'IDR',
 			  		amount: total,
 			  		checkoutMethod: 'ONE_TIME_PAYMENT',
 			  		channelCode: 'ID_DANA',
 			  		channelProperties: {
- 	   					success_redirect_url: 'http://localhost:3000/payment/callback',
+ 	   					success_redirect_url: 'http://localhost:3000/api/payment/callback',
   					}
-			}) 
+				})
+				break
 		}
 		return charge
 		}catch(error){
